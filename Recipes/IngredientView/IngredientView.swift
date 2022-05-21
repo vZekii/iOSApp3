5c4e5b342cdc2a0ce5445a42392f516d3c85d088
Bailey Mendonca <mendoncabailey01@gmail.com>
2022-05-21 18:19:26 +1000

    Added a label so tutor knows the page can be refreshed with this button

--- Recipes/IngredientView/IngredientView.swift
+++ Recipes/IngredientView/IngredientView.swift
@@ -105,6 +105,7 @@ struct IngredientView: View {
                         searchText = "Updating"
                         searchText = ""
                     } label: {
+                        Text("Refresh")
                         Image(systemName: "repeat")
                     }
                 }
