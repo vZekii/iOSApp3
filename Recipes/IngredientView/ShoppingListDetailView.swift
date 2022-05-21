b78821ade31b13eecfea4596f895e4d62e9c8dcf
Bailey Mendonca <mendoncabailey01@gmail.com>
2022-05-21 18:10:08 +1000

    Added comments and fixed most styling (I hope)

--- Recipes/IngredientView/ShoppingListDetailView.swift
+++ Recipes/IngredientView/ShoppingListDetailView.swift
@@ -10,32 +10,39 @@ import SwiftUI
 
 struct ShoppingListDetailView: View {
     struct EntryRow: View {
+        //state checks if the value has been striked to action the code accordingly
         @State private var striked = false
         var entry: ShoppingList
         var body: some View {
-            Text("\(entry.measurement.getMeasurement()) \(entry.ingredient.name) for entry \(entry.id)")
+            //shows the entry
+            Text("\(entry.measurement.getMeasurement()) \(entry.ingredient.name)")
                 .strikethrough(striked)
+                //adds a strike through if it is tapped
                 .onTapGesture {
+                    //once it is tapped it changes the striked bool
                     self.striked.toggle()
+                    //runs code if a strike is through the item
                     if striked {
-                    ShoppingList.sampleData.remove(at: ShoppingList.sampleData.firstIndex(where: {$0.id == entry.id})!)
-                        
-                    let ingredientToTopUp: Int = Ingredient.sampleData.firstIndex(where: {$0.name == entry.ingredient.name}) ?? -1
-                    //Tops it up in ingredients.
-                    if(ingredientToTopUp != -1 ) {
-                        
-                        Ingredient.sampleData[ingredientToTopUp].currentAmount.amount = Ingredient.sampleData[ingredientToTopUp].lastBoughtAmount.amount
-                    }
+                        //removes the item from the shoppinglist array
+                        ShoppingList.sampleData.remove(at: ShoppingList.sampleData.firstIndex(where: {$0.id == entry.id})!)
+                            //checks if the ingredient is in the list
+                        let ingredientToTopUp: Int = Ingredient.sampleData.firstIndex(where: {$0.name == entry.ingredient.name}) ?? -1
+                        //Tops it up in ingredients.
+                        if(ingredientToTopUp != -1 ) {
+                            //if the item is found tops up the item
+                            Ingredient.sampleData[ingredientToTopUp].currentAmount.amount = Ingredient.sampleData[ingredientToTopUp].lastBoughtAmount.amount
+                        }
+                        else {
+                            //adds the ingredient in the case that it is not there
+                            let lastChecker: Ingredient = Ingredient.sampleData.last!
+                            let id = (lastChecker.id + 1)
+                            var holderIngredient = entry.ingredient
+                            holderIngredient.id = id
+                            ingAdder(ingredient: holderIngredient)
+                        }
+                        }
+                    //if the item is unstriked it adds the item back to the list in case an item is accidentally striked
                     else {
-                        let lastChecker: Ingredient = Ingredient.sampleData.last!
-                        let id = (lastChecker.id + 1)
-                        var holderIngredient = entry.ingredient
-                        holderIngredient.id = id
-                        ingAdder(ingredient: holderIngredient)
-                    }
-                    }
-                    else {
-                        
                         ShoppingList.sampleData.append(entry)
                     }
                     
@@ -54,6 +61,7 @@ struct ShoppingListDetailView: View {
                     
         }
         .listStyle(.grouped)
+        //sets nav title
         .navigationTitle("Shopping List")
     }
 }
